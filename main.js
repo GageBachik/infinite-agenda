d27470cb336699c00ccfcccb2089e1ce9d7e89cc
gbachik <gbachik@gmail.com>
2014-07-02 12:23:45 -0600

    Finished one days worth of egends

--- main.js
+++ main.js
@@ -1,18 +1,40 @@
 var count = 1;
 function addAppoint(){
-	var task = $('<p class="test" contenteditable="true" >Click to edit and, press enter to save.</p>').css({maxWidth: '60%'});
+	var task = $('<p class="editable" contenteditable="true" ></p>').css({maxWidth: '60%', color: 'gray'});
 	$(arguments[0]).after(task);
 	count++;
 }
 
 $(document).on('ready', function() {
+	$('.day').hover(function(){
+		$(this).find('.add-item').toggle();	
+	}, function(){
+		$(this).find('.add-item').toggle();
+	});
 	$(document).on('click', '.add-item', function(event){
 		addAppoint(this);
+		$('.editable:first').focus();
+		$('.editable:first').blur(function(event){
+			if($('.editable:first').text() === ''){
+				$('.editable:first').remove();
+			}else{
+				$('.editable:first').attr({contenteditable: false});
+        		$('.editable:first').css('color', 'black');
+			}
+		});
 	});
 	$(document).keypress('p', function(event){
 		if(event.which === 13) {
-        	$('p').attr({contenteditable: false});
-        	console.log('test');
+			if($('p:first').text() !== ''){
+				$('p').attr({contenteditable: false});
+        		$('p').css('color', 'black');
+			}else{
+				$('p:first').remove();
+			}
     	}
+	});
+
+	$(document).on('click', function() {
+
 	});
 });
